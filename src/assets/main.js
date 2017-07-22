a6faee40348234475bc561cd25839521fb351561
OWalls <owalls1203@gmail.com>
2017-07-22 17:35:18 -0400

    div and title

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,36 @@
 $(function() {
 
   // your code will go here
+  /* #1 - Make an Ajax Call
+ Make an Ajax request to the Report Card URL you found on CodeSchool.com - Set the dataType as jsonp - Pass a success callback.*/
+  $.ajax({
+   url: 'https://www.codeschool.com/users/owalls.json',
+   dataType: 'jsonp',
+   success: function(response) {
+     // handle response
+     console.log('response', response.courses.completed)
+     addCourses(response.courses.completed)
+   }
+ });
 
+function addCourses(courses) {
+  var $badges = $('#badges');
+
+courses.forEach(function(course) {
+
+  var $course =   $('<div />', {
+      'class': 'course'
+    }).appendTo($badges);
+
+    $('<h3 />', {
+      text: course.title
+    }).appendTo($course);
+
+
+
+  })
+}
+
+
+//Main Function closing below
 });
